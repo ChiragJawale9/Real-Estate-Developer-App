817f580a61cb93a17186adbbfc6e3af22885dca2
Abhinav Sawant <36274688+abhi16598@users.noreply.github.com>
2021-03-19 22:32:55 +0530

    First commit

---
+++ src/main/java/com/reba/RealEstateDeveloperAppApplication.java
@@ -0,0 +1,13 @@
+package com.reba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateDeveloperAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateDeveloperAppApplication.class, args);
+	}
+
+}
